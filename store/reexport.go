e84495e9cb6783a82736616ec5f6614fdc811106
tjan <earltjan@foxmail.com>
2019-12-06 17:29:21 +0800

    Export internal type definition

--- store/reexport.go
+++ store/reexport.go
@@ -8,28 +8,29 @@ import (
 // Import cosmos-sdk/types/store.go for convenience.
 // nolint
 type (
-	PruningOptions   = types.PruningOptions
-	Store            = types.Store
-	Committer        = types.Committer
-	CommitStore      = types.CommitStore
-	MultiStore       = types.MultiStore
-	CacheMultiStore  = types.CacheMultiStore
-	CommitMultiStore = types.CommitMultiStore
-	KVStore          = types.KVStore
-	KVPair           = types.KVPair
-	Iterator         = types.Iterator
-	CacheKVStore     = types.CacheKVStore
-	CommitKVStore    = types.CommitKVStore
-	CacheWrapper     = types.CacheWrapper
-	CacheWrap        = types.CacheWrap
-	CommitID         = types.CommitID
-	StoreKey         = types.StoreKey
-	StoreType        = types.StoreType
-	Queryable        = types.Queryable
-	TraceContext     = types.TraceContext
-	Gas              = stypes.Gas
-	GasMeter         = types.GasMeter
-	GasConfig        = stypes.GasConfig
+	PruningOptions           = types.PruningOptions
+	Store                    = types.Store
+	Committer                = types.Committer
+	CommitStore              = types.CommitStore
+	MultiStore               = types.MultiStore
+	CacheMultiStore          = types.CacheMultiStore
+	CommitMultiStore         = types.CommitMultiStore
+	KVStore                  = types.KVStore
+	KVPair                   = types.KVPair
+	Iterator                 = types.Iterator
+	CacheKVStore             = types.CacheKVStore
+	CommitKVStore            = types.CommitKVStore
+	ForkableCommitMultiStore = types.ForkableCommitMultiStore
+	CacheWrapper             = types.CacheWrapper
+	CacheWrap                = types.CacheWrap
+	CommitID                 = types.CommitID
+	StoreKey                 = types.StoreKey
+	StoreType                = types.StoreType
+	Queryable                = types.Queryable
+	TraceContext             = types.TraceContext
+	Gas                      = stypes.Gas
+	GasMeter                 = types.GasMeter
+	GasConfig                = stypes.GasConfig
 )
 
 // nolint - reexport
